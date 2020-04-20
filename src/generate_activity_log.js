9a8070b31c58bd267e74d07ede72e50b2238fdcd
Jun <junaidvayoli@gmail.com>
2020-04-21 01:58:06 +0900

    typo again

--- src/generate_activity_log.js
+++ src/generate_activity_log.js
@@ -4,7 +4,7 @@ const moment = require('moment-timezone');
 const data = require('../tmp/data.json');
 const data_prev = require('../tmp/data_prev.json');
 
-update_log_file = './tmp/updatelog/log.json';
+update_log_file = '../tmp/updatelog/log.json';
 var update_log = require(update_log_file);
 BOT_TOKEN = process.argv[2];
 
